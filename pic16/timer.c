fe998ae24da01ee025f0fb9feb6c6c2c646d07a4
Mattias Buelens <mattias@buelens.com>
2013-11-17 15:41:08 +0100

    Added timer library. (WIP)

--- pic16/timer.c
+++ pic16/timer.c
@@ -9,14 +9,7 @@
 #include "../platform/timer.h"
 
 void timer_init(void) {
-	// Use 8-bit operation
-	T0CONbits.T08BIT = 1;
-	// Use internal instruction cycle clock
-	T0CONbits.T0CS = 0;
-	// Don't use the prescaler
-	T0CONbits.PSA = 1;
-	// Enable interrupts
-	INTCONbits.TMR0IE = 1;
+
 }
 
 void timer_handle_interrupt(void) {
@@ -36,6 +29,10 @@ void timer_set_active(BOOL active) {
 }
 
 void timer_set_handler(TIMER_HANDLER handler) {
+
+}
+
+void timer_set_prescaler(BYTE scale) {
 
 }
 
